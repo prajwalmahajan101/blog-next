772678475a49d8c52aea27ef64fb856f5bcb78f8
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-05-29 23:14:35 +0530

    Code and images in md files

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,27 @@
 import PostContent from "@/components/posts/post-detail/PostContent";
+import { getAllPostPaths, getPostData } from "@/lib/post-utils";
 
-const PostDetailsPage = () => {
-  return <PostContent />;
+const PostDetailsPage = ({ post }) => {
+	return <PostContent post={post} />;
+};
+
+export const getStaticProps = (ctx) => {
+	const { slug } = ctx.params;
+	const post = getPostData(slug);
+	return {
+		props: {
+			post,
+		},
+		revalidate: 24 * 60 * 60,
+	};
+};
+
+export const getStaticPaths = () => {
+	const postFiles = getAllPostPaths();
+	const slugs = postFiles.map((file) => file.replace(/\.md$/, ""));
+	return {
+		paths: slugs.map((slug) => ({ params: { slug } })),
+		fallback: false,
+	};
 };
 export default PostDetailsPage;
