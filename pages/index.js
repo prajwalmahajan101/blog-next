7605848482bb8e0cdc1b9b9513f89e06a0dce45f
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-05-29 21:00:29 +0530

    error solved

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,22 @@ import FeaturedPost from "@/components/home-page/FeaturedPost";
 import { getFeaturedPosts } from "@/lib/post-utils";
 
 const HomePage = ({ featuredPosts }) => {
-  return (
-    <>
-      <Hero />
-      <FeaturedPost featuredPosts={featuredPosts} />
-    </>
-  );
+	return (
+		<>
+			<Hero />
+			<FeaturedPost featuredPosts={featuredPosts} />
+		</>
+	);
 };
 
-export const getStaticPost = async () => {
-  const featuredPosts = getFeaturedPosts();
-  return {
-    props: {
-      featuredPosts,
-    },
-    revalidate: 24 * 60 * 60,
-  };
+export const getStaticProps = async () => {
+	const featuredPosts = getFeaturedPosts();
+	return {
+		props: {
+			featuredPosts,
+		},
+		revalidate: 24 * 60 * 60,
+	};
 };
 
 export default HomePage;
