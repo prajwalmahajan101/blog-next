cc866ba0de7202be4f39a1839714b36cccfcccdb
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-05-29 19:15:03 +0530

    till now

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,24 @@
 import Hero from "@/components/home-page/Hero";
 import FeaturedPost from "@/components/home-page/FeaturedPost";
+import { getFeaturedPosts } from "@/lib/post-utils";
 
-const Dummy_post = [
-  {
-    title: "Getting Started With Next Js",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is the React framework for production - its makes building fullstack React apps and sites a breeze and ships with built-in server side rendering (SSR)",
-    date: "2002-02-10",
-    slug: "getting-started-with-nextjs-1",
-  },
-  {
-    title: "Getting Started With Next Js",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is the React framework for production - its makes building fullstack React apps and sites a breeze and ships with built-in server side rendering (SSR)",
-    date: "2002-02-10",
-    slug: "getting-started-with-nextjs-2",
-  },
-  {
-    title: "Getting Started With Next Js",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is the React framework for production - its makes building fullstack React apps and sites a breeze and ships with built-in server side rendering (SSR)",
-    date: "2002-02-10",
-    slug: "getting-started-with-nextjs-3",
-  },
-  {
-    title: "Getting Started With Next Js",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJs is the React framework for production - its makes building fullstack React apps and sites a breeze and ships with built-in server side rendering (SSR)",
-    date: "2002-02-10",
-    slug: "getting-started-with-nextjs-4",
-  },
-];
-
-const HomePage = () => {
+const HomePage = ({ featuredPosts }) => {
   return (
     <>
       <Hero />
-      <FeaturedPost featuredPosts={Dummy_post} />
+      <FeaturedPost featuredPosts={featuredPosts} />
     </>
   );
 };
+
+export const getStaticPost = async () => {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      featuredPosts,
+    },
+    revalidate: 24 * 60 * 60,
+  };
+};
+
 export default HomePage;
