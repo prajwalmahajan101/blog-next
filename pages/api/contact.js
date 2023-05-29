ea430b9cbae100dd310b336daa8f39da625d88a4
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-05-30 00:05:09 +0530

    Db Added to contact api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
 	const { method } = req;
 	if (method === "POST") {
 		const { email, name, message } = req.body;
@@ -16,11 +18,36 @@ const handler = (req, res) => {
 				message: "InValid Inut",
 			});
 		}
+		try {
+			const client = await MongoClient.connect(process.env.DB_URL);
+			const messagesCollection = client.db().collection("messages");
+		} catch (err) {
+			return res.status(500).json({
+				status: fasle,
+				message: "Could Not Connect To Database",
+			});
+		}
+
+		try {
+			const new_message = await messagesCollection.insertOne({
+				email,
+				name,
+				message,
+			});
+		} catch (err) {
+			client.close();
+			return res.status(500).json({
+				status: fasle,
+				message: "Could Not Create the Entry In Database",
+			});
+		}
+		client.close();
 
 		return res.status(201).json({
 			status: true,
 			message: "Message Successfully stored",
 			data: {
+				id: new_message.insertedId,
 				email,
 				name,
 				message,
